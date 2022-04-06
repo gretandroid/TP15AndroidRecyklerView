b580479759b24a7e1e9c37d9d23323ab362bb17a
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-06 10:14:30 +0200

    cleanup code

--- app/src/main/java/education/cccp/mobile/CustomAdapter.kt
+++ app/src/main/java/education/cccp/mobile/CustomAdapter.kt
@@ -1,14 +1,23 @@
 package education.cccp.mobile
 
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
 
-class CustomAdapter: RecyclerView.Adapter<TextViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextViewHolder {
+@Suppress("unused")
+class CustomAdapter: Adapter<TextViewHolder>() {
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TextViewHolder {
         TODO("Not yet implemented")
     }
 
-    override fun onBindViewHolder(holder: TextViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: TextViewHolder,
+        position: Int
+    ) {
         TODO("Not yet implemented")
     }
 
