0e2211b0e452f5f225ebdb70aefbdb9233111256
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-07 11:28:21 +0200

    un petit commit de plus

--- app/src/main/java/education/cccp/mobile/model/Person.kt
+++ app/src/main/java/education/cccp/mobile/model/Person.kt
@@ -14,12 +14,12 @@ data class Person(
     companion object {
         private const val serialVersionUID = 1L
         const val TABLE_PERSON = "table_person"
-        private const val TABLE_PERSON_COL_ID = "id"
-        private const val TABLE_PERSON_COL_FIRST_NAME = "first_name"
-        private const val TABLE_PERSON_COL_LAST_NAME = "last_name"
-        private const val TABLE_PERSON_COL_EMAIL = "email"
-        private const val TABLE_PERSON_COL_NICKNAME = "nickname"
-        private const val TABLE_PERSON_COL_DATE_OF_BIRTH = "date_of_birth"
+        const val TABLE_PERSON_COL_ID = "id"
+        const val TABLE_PERSON_COL_FIRST_NAME = "first_name"
+        const val TABLE_PERSON_COL_LAST_NAME = "last_name"
+        const val TABLE_PERSON_COL_EMAIL = "email"
+        const val TABLE_PERSON_COL_NICKNAME = "nickname"
+        const val TABLE_PERSON_COL_DATE_OF_BIRTH = "date_of_birth"
 
         const val CREATE_TABLE_PERSON =
             """CREATE TABLE $TABLE_PERSON 
